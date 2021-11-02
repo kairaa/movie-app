c8cc3a8364f9c66b7ea68a850f666db64075f3ed
Kayra Çakıroğlu <1030510099@erciyes.edu.tr>
2021-11-02 16:58:55 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const https = require('http');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+
+app.post('/', (req, res) => {
+  const movieName = req.body.movieName;
+  const apiKey = 'cfd6f31c&t';
+  let url = `http://www.omdbapi.com/?apikey=${apiKey}&t=${movieName}`
+  console.log(url);
+
+  https.get(url, (response) => {
+    console.log(response.statusCode);
+    if (response.statusCode === 200) {
+      response.on('data', (data) => {
+        let movieData = JSON.parse(data);
+        if(movieData.Response === 'True'){
+          res.render('movie', {
+            movieInfo: {
+              movieName: movieData.Title,
+              poster: movieData.Poster,
+              year: movieData.Year,
+              genre: movieData.Genre,
+              director: movieData.Director,
+              metascore: movieData.Metascore,
+              imdb: movieData.imdbRating,
+            }
+          })
+        }
+        else{
+          res.render('notFound');
+        }
+      })
+    }
+  })
+
+})
+
+app.post('/movie', (req, res) => {
+  res.redirect('/');
+})
+
+app.get('/about', (req, res) => {
+  res.render('about');
+})
+
+app.get('/movie', (req, res) => {
+  res.render('movie', { movieName: "Deneme" });
+})
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+})
