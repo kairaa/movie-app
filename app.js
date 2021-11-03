5b0cfcea2bc52a029fd3ead83de8e39a4706ee00
Kayra Çakıroğlu <1030510099@erciyes.edu.tr>
2021-11-03 18:38:14 +0300

    unexpected end of JSON input hatasi duzeltildi

--- app.js
+++ app.js
@@ -15,33 +15,41 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   const movieName = req.body.movieName.split(' ').join('+');;
-  const apiKey = 'cfd6f31c&t';
+  const apiKey = 'cfd6f31c';
   let url = `https://www.omdbapi.com/?apikey=${apiKey}&t=${movieName}`
   console.log(url);
 
   https.get(url, (response) => {
+    const chunks = [];
     console.log(response.statusCode);
     if (response.statusCode === 200) {
-      response.on('data', (data) => {
-        console.log(data);
-        let movieData = JSON.parse(data);
-        if(movieData.Response === 'True'){
-          res.render('movie', {
-            movieInfo: {
-              movieName: movieData.Title,
-              poster: movieData.Poster,
-              year: movieData.Year,
-              genre: movieData.Genre,
-              director: movieData.Director,
-              metascore: movieData.Metascore,
-              imdb: movieData.imdbRating,
-              plot: movieData.Plot,
-            }
-          })
-        }
-        else{
-          res.render('notFound');
-        }
+      //https://stackoverflow.com/questions/62480360/error-syntaxerror-unexpected-end-of-json-input
+      response.on('data', (chunk) => {
+        chunks.push(chunk);
+        // console.log(chunk);
+
+        response.on('end', () =>{
+          const data = Buffer.concat(chunks);
+          var got = JSON.parse(data);
+
+          if(got.Response === 'True'){
+            res.render('movie', {
+              movieInfo: {
+                movieName: got.Title,
+                poster: got.Poster,
+                year: got.Year,
+                genre: got.Genre,
+                director: got.Director,
+                metascore: got.Metascore,
+                imdb: got.imdbRating,
+                plot: got.Plot,
+              }
+            })
+          }
+          else{
+            res.render('notFound');
+          }
+        })
       })
     }
   })
